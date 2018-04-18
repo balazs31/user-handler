049d29dfbaf6b550564cff4c3f24db018e340454
kbim1434 <kendi.balazs41@gmail.com>
2018-04-18 12:56:35 +0300

    Add navbar and table components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserTableComponent } from './user-table/user-table.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserTableComponent
   ],
   imports: [
     BrowserModule
